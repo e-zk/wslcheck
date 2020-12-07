88046c067acb0b40e65ecc62151e0c30a9316a63
e-zk <58356365+e-zk@users.noreply.github.com>
2020-12-07 21:43:42 +1000

    add godoc comments

--- wslcheck.go
+++ wslcheck.go
@@ -1,5 +1,13 @@
+// The wslcheck package provides a simple function that checks whether the
+// current Linux environment is in fact the Windows Subsystem for Linux (WSL).
 package wslcheck
 
+// Check returns true if the Linux version string matches that of a
+// WSL distribution. Otherwise (including when the host OS is not Linux
+// at all), it returns false.
+//
+// If an error occurs when attempting to get the version string, Check will
+// return false and the error value will be populated (not-nil).
 func Check() (bool, error) {
 	return check()
 }
