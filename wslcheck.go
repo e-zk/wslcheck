e55d66fed1a3d747664d84e35a03b99a094f22ea
e-zk <e-zk@users.noreply.github.com>
2021-01-28 23:33:34 +1000

    add constants and godoc to wslcheck.go
    * clean up wslcheck_unix.go
    * use global constants in wslcheck_unix.go

--- wslcheck.go
+++ wslcheck.go
@@ -2,6 +2,11 @@
 // current Linux environment is in fact the Windows Subsystem for Linux (WSL).
 package wslcheck
 
+const (
+	ProcReleasePath = "/proc/sys/kernel/osrelease" // Path to `/proc` pseudo-file that shows release info
+	VersionRegexp   = `.*microsoft-standard.*`     // Regular expression used to test against the output of ProcReleasePath
+)
+
 // Check returns true if the Linux version string matches that of a
 // WSL distribution. Otherwise (including when the host OS is not Linux
 // at all), it returns false.
--- wslcheck_unix.go
+++ wslcheck_unix.go
@@ -6,22 +6,15 @@ import (
 	"runtime"
 )
 
-const (
-	linuxReleaseProc = "/proc/sys/kernel/osrelease"
-)
-
 func check() (bool, error) {
 	runt := runtime.GOOS
-	r := regexp.MustCompile(`.*microsoft-standard.*`)
+	r := regexp.MustCompile(VersionRegexp)
 
 	if runt == "linux" {
-		verBytes, err := ioutil.ReadFile(linuxReleaseProc)
+		verBytes, err := ioutil.ReadFile(ProcReleasePath)
 		if err != nil {
 			return false, err
 		}
-		//if strings.HasSuffix(string(verBytes), "microsoft-standard\n") {
-		//	return true, nil
-		//}
 		if r.Match(verBytes) {
 			return true, nil
 		}
